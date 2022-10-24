fc311d5df9cf49f6b8dcc6d2c9299975c865a3fa
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-25 00:46:54 +0300

    PW12 - new project

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,34 @@
+import React from "react";
 import ButtonSubmit from "../ButtonSubmit/ButtonSubmit";
+import Input from "../Input/Input";
 
 export default function Login() {
+  const [login, setLogin] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  function handleLoginChange(evt) {
+    setLogin(evt.target.value);
+  }
+
+  function handleEmailChange(evt) {
+    setEmail(evt.target.value);
+  }
+
   return (
     <>
-      <h2 className="login__title">Вход</h2>
-      <form className="login__form">
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Пароль" />
+      <h2>Вход</h2>
+      <form>
+        <Input
+          onChange={handleLoginChange}
+          value={login}
+          type={"email"}
+          placeholder={"Email"} />
+        <Input
+          onChange={handleEmailChange}
+          value={email}
+          type={"password"}
+          placeholder={"Пароль"}
+        />
         <ButtonSubmit color="white">{"Войти"}</ButtonSubmit>
       </form>
     </>
