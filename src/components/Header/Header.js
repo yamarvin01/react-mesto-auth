25927e3dcb350596d3c70005ebd0b5f51172c88c
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-26 02:34:53 +0300

    PW12 - new project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,24 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../styles/images/logo-mesto.svg";
+import { useRouteMatch } from "react-router-dom";
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">
-        <a className="header__link" href="#" target="_blank" rel="noopener">
-          <img className="header__logo" src={logo} alt="Логотип Mesto Russia" />
-        </a>
-        <div>
-          <Link className="header__link" to="/sign-in">
-            Войти
-          </Link>
-          <Link className="header__link" to="/sign-up">
-            Регистрация
-          </Link>
-        </div>
-      </header>
-    );
-  }
+export default function Header() {
+  const { url, path } = useRouteMatch();
+  console.log("url: " + url, "path: " + path);
+
+  return (
+    <header className="header">
+      <a className="header__link" href="#" target="_blank" rel="noopener">
+        <img className="header__logo" src={logo} alt="Логотип Mesto Russia" />
+      </a>
+      <div>
+        <Link className="header__link" to="/sign-in">
+          Войти
+        </Link>
+        <Link className="header__link" to="/sign-up">
+          Регистрация
+        </Link>
+      </div>
+    </header>
+  );
 }
