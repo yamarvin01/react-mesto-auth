4ff4230319c1b36c37bb3cad59206146cff0bb3a
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-25 22:13:39 +0300

    PW12 - new project

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ButtonSubmit from "../ButtonSubmit/ButtonSubmit";
-import Input from "../Input/Input";
-import Form from "../Form/Form";
 
 export default function Login() {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
   function handleEmailChange(evt) {
     setEmail(evt.target.value);
@@ -19,32 +17,48 @@ export default function Login() {
     evt.preventDefault();
     console.log({
       email: email,
-      password: password
+      password: password,
     });
   }
 
   return (
     <div className="login">
       <h2 className="login__title">Вход</h2>
-      <Form
-        onSubmit={handleSubmit}
-        name={"login"}
-      >
-        <Input
-          onChange={handleEmailChange}
-          value={email}
-          name={"email"}
-          type={"email"}
-          placeholder={"Email"} />
-        <Input
-          onChange={handlePasswordChange}
-          value={password}
-          name={"password"}
-          type={"password"}
-          placeholder={"Пароль"}
-        />
-        <ButtonSubmit type="secondary">{"Войти"}</ButtonSubmit>
-      </Form>
+      <form className="login__form" onSubmit={handleSubmit} name={"login"}>
+        <label htmlFor="email">
+          <input
+            className="login__input"
+            onChange={handleEmailChange}
+            value={email}
+            name="email"
+            type="email"
+            placeholder="Email"
+            id="email"
+            required
+          />
+          <span className="email-input-error popup__error"></span>
+        </label>
+        <label htmlFor="password">
+          <input
+            className="login__input"
+            onChange={handlePasswordChange}
+            value={password}
+            name="password"
+            type="password"
+            placeholder="Пароль"
+            id="password"
+            required
+          />
+          <span className="password-input-error popup__error"></span>
+        </label>
+        <button
+          className="button button_type_submit button_type_secondary"
+          type="submit"
+          aria-label="Войти"
+        >
+          Войти
+        </button>
+      </form>
     </div>
   );
 }
