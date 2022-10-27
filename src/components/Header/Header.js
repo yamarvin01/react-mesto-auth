e68468d41a590d9bf4a93878e7169fb3bda07591
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-27 23:14:26 +0300

    PW12 - new project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../styles/images/logo-mesto.svg";
 import { useHistory } from "react-router-dom";
