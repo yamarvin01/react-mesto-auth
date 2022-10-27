56ecec1997e9181cdc1d31efcda84c1cde0f26f5
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-28 01:19:04 +0300

    PW12 - new project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,7 +19,7 @@ export default function Header(props) {
   }, [history]);
 
   function handleClick() {
-    setIsEmailVisible(true);
+    setIsEmailVisible(!isEmailVisible);
   }
 
   return (
@@ -29,7 +29,7 @@ export default function Header(props) {
         <a className="header__link" href="#" target="_blank" rel="noopener">
           <img className="header__logo" src={logo} alt="Логотип Mesto Russia" />
         </a>
-        <div className="header__content header__content_hidden">
+        <div className="header__content">
           <p className="header__text">{currentUserEmail}</p>
           {pathName === "/sign-up" && (
             <Link className="header__link" to="/sign-in">Войти</Link>
@@ -44,12 +44,28 @@ export default function Header(props) {
       </header>
     }
 
+    {width <= 700 && props.loggedIn && isEmailVisible &&
+      <div className="header__mobile">
+        <p className="header__mobile-text">{currentUserEmail}</p>
+        <Link
+          to="sign-in"
+          onClick={props.signOut}
+          className="header__mobile-link"
+        >
+          Выйти
+        </Link>
+      </div>
+    }
+
     {width <= 700 && props.loggedIn &&
       <header className="header">
         <a className="header__link" href="#" target="_blank" rel="noopener">
           <img className="header__logo" src={logo} alt="Логотип Mesto Russia" />
         </a>
-        <img className="header__icon-group" onClick={handleClick} src={require(`../../styles/images/icon-group.png`)} />
+        <img
+          className={isEmailVisible ? "header__icon-close" : "header__icon-group"}
+          src={isEmailVisible ? require(`../../styles/images/btn-close.png`) : require(`../../styles/images/icon-group.png`)}
+          onClick={handleClick} />
       </header>
     }
     </>
