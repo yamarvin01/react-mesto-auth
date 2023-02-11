849e911bd982f13b57b200dc3216ff4f61440e1c
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-02-11 16:33:28 +0300

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -84,7 +84,7 @@ class Api {
 }
 
 export const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-49",
+  baseUrl: "http://api.yamarvin01.nomoredomainsclub.ru",
   headers: {
     authorization: "37ded591-0952-406f-9bd6-1d8027d482f6",
     "Content-Type": "application/json",
